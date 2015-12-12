143a205567cb8417f6a8a7ccaaa21c6da63c08ca
Harald Kraemer <tetha42@gmail.com>
2015-12-12 17:25:54 +0100

    Snap shop tiles back

--- src/scripts/state/PlantBuysStuff.ts
+++ src/scripts/state/PlantBuysStuff.ts
@@ -22,18 +22,30 @@ module Ld34.State {
       this.leafShop.input.enableDrag(true);
       this.leafShop.events.onDragStart.add(this.highlightFieldsCloseToPlants, this);
       this.leafShop.events.onDragStop.add(this.resetHighlights, this);
+      this.leafShop.events.onDragStop.add(() => {
+        this.leafShop.x = 600;
+        this.leafShop.y = 100;
+      });
 
       this.rockDrillerShop = this.add.sprite(600, 200, 'rockDriller');
       this.rockDrillerShop.inputEnabled = true;
       this.rockDrillerShop.input.enableDrag(true);
       this.rockDrillerShop.events.onDragStart.add(this.highlightFieldsCloseToPlants, this);
       this.rockDrillerShop.events.onDragStop.add(this.resetHighlights, this);
+      this.rockDrillerShop.events.onDragStop.add(() => {
+        this.rockDrillerShop.x = 600;
+        this.rockDrillerShop.y = 200;
+      });
 
       this.manEaterShop = this.add.sprite(600, 300, 'manEater');
       this.manEaterShop.inputEnabled = true;
       this.manEaterShop.input.enableDrag(true);
       this.manEaterShop.events.onDragStart.add(this.highlightFieldsCloseToPlants, this);
       this.manEaterShop.events.onDragStop.add(this.resetHighlights, this);
+      this.manEaterShop.events.onDragStop.add(() => {
+        this.manEaterShop.x = 600;
+        this.manEaterShop.y = 300;
+      });
     }
 
     highlightFieldsCloseToPlants() {
