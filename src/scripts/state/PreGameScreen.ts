17ccd46d7a3350641aa880f9f9aa83d5267c4ce5
Harald Kraemer <tetha42@gmail.com>
2015-12-13 11:15:57 +0100

    Add documentation on the pre game screen

--- src/scripts/state/PreGameScreen.ts
+++ src/scripts/state/PreGameScreen.ts
@@ -4,6 +4,32 @@ module Ld34.State {
 
     create() {
       this.add.button(600, 50, 'startButton', this.startGame, this);
+      this.add.text(50, 75, "You are a strange alien plant.\n");
+      this.add.text(50, 150, "Your goal is to place a man-eating\n"
+                          + "monster-plant next to each town!");
+
+      this.add.sprite(650, 150, 'town');
+      this.add.sprite(700, 150, 'manEater');
+
+      this.add.text(50, 250, "To do so, place leafs around to cover space,\n"
+                           + "since you can only place plants next\n"
+                           + "to existing plants");
+
+      this.add.sprite(700, 200, 'leaf');
+      this.add.sprite(700, 250, 'leaf');
+      this.add.sprite(700, 300, 'sapling');
+
+      this.add.text(50, 410, "Place Rock Drillers to get resources from rocks");
+
+      this.add.sprite(700, 350, 'leaf');
+      this.add.sprite(700, 400, 'rockDriller');
+      this.add.sprite(750, 400, 'rock');
+
+      this.add.text(50, 475, "And place man-eating soldiers to eat soldiers");
+
+      this.add.sprite(750, 450, 'manEater');
+      this.add.sprite(700, 500, 'soldier');
+
     }
 
     startGame() {
