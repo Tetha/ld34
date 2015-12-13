d828875ac05c1117881e446e4412d5bc401bf673
Harald Kraemer <tetha42@gmail.com>
2015-12-13 17:28:05 +0100

    Added ingame description of stuff

--- src/scripts/state/PlantBuysStuff.ts
+++ src/scripts/state/PlantBuysStuff.ts
@@ -26,15 +26,23 @@ module Ld34.State {
         this.gridSprites[r][c] = sprite;
       });
 
-      this.leafShop = this.add.sprite(600, 100, 'leaf');
-      this.rockDrillerShop = this.add.sprite(600, 200, 'rockDriller');
-      this.manEaterShop = this.add.sprite(600, 300, 'manEater');
+      this.leafShop = this.add.sprite(525, 100, 'leaf');
+      this.rockDrillerShop = this.add.sprite(525, 200, 'rockDriller');
+      this.manEaterShop = this.add.sprite(525, 300, 'manEater');
 
-      this.setupSpriteOnce(this.leafShop, 100, leafCost);
-      this.setupSpriteOnce(this.rockDrillerShop, 200, drillCost);
-      this.setupSpriteOnce(this.manEaterShop, 300, manEaterCost, true);
+      this.setupSpriteOnce(this.leafShop, 100, leafCost,
+        "Leaf: Doesn't do much in itself,\n"
+       +"except allow you to spread further");
+      this.setupSpriteOnce(this.rockDrillerShop, 200, drillCost,
+        "Drill: Produces Evo-Points next\n"
+       +"to a rock");
+      this.setupSpriteOnce(this.manEaterShop, 300, manEaterCost, 
+        "Man-Eater: Destroys soldiers next to\n"
+       +"itself and locks down towns next\n"
+       +"to itself",
+        true);
 
-      this.add.button(600, 500, 'endTurnButton', this.endTurn, this);
+      this.add.button(525, 450, 'endTurnButton', this.endTurn, this);
       this.evoPointDisplay = this.add.text(525, 25, "EvoPoints: " + this.game.evoPoints, {
         'font' : '14pt Arial'
       });
@@ -73,7 +81,7 @@ module Ld34.State {
       sprite.loadTexture(this.game.getField(row, col));
     }
 
-    setupSpriteOnce(sprite: Phaser.Sprite, y:number, cost:number, checkManEater = false) {
+    setupSpriteOnce(sprite: Phaser.Sprite, y:number, cost:number, description: string, checkManEater = false) {
         sprite.inputEnabled = true;
         sprite.input.enableDrag(true);
         sprite.events.onDragStart.add(this.highlightFieldsCloseToPlants, this);
@@ -100,10 +108,13 @@ module Ld34.State {
         });
         sprite.events.onDragStop.add(this.resetHighlights, this);
         sprite.events.onDragStop.add(() => {
-          sprite.x = 600;
+          sprite.x = 525;
           sprite.y = y;
         });
-        this.add.text(650, y, "Cost: " + cost);
+        this.add.text(575, y, "Cost: " + cost);
+        this.add.text(525, y + 50, description, {
+          'font' : '12pt Arial'
+        });
     }
 
     enableOrDisableShopSprite(sprite:Phaser.Sprite, cost:number) {
